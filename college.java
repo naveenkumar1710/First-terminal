51ae498b9f30cc1f49be9cb238a4516cd6608cf7
naveenkumar1710 <naveenkumarv3212@gmail.com>
2022-10-28 10:26:54 +0530

    naveen

---
+++ college.java
@@ -0,0 +1,12 @@
+public class college{
+static int totalbus=20;
+int totalstudent=30;
+static void listenclass(){
+
+System.out.println("listening the class");
+}
+public static void main(String[]args){
+college.listenclass();
+System.out.println(totalbus);
+}
+}
