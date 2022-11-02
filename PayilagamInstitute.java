7916e16e4654769221b0f53ee3a3414e463318dd
naveenkumar1710 <naveenkumarv3212@gmail.com>
2022-11-02 18:41:37 +0530

    Constructor Chaining

---
+++ PayilagamInstitute.java
@@ -0,0 +1,9 @@
+                    //Constructor Chaining
+public class PayilagamInstitute{
+             PayilagamInstitute(){
+                System.out.println("Team Payilagam");
+             }
+             PayilagamInstitute(int a,int y,String c){
+                System.out.println(a+"\n"+y+"\n"+c);
+             }
+}
