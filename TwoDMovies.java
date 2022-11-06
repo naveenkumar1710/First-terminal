131fae2e2bb43c97027265b9e2acf2953b731b14
naveenkumar1710 <naveenkumarv3212@gmail.com>
2022-11-06 21:11:12 +0530

    Super keyword using on the method

---
+++ TwoDMovies.java
@@ -0,0 +1,10 @@
+                                  //Super keyword using on method
+public class TwoDMovies{
+         int noOfAds=3;
+        void watchingMovie(){
+            System.out.println("I am watching 2d movie");
+        }
+        void playAds(){
+            System.out.println("playing 2D Ads");
+        }
+}
